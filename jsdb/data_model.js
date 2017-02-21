bbcb11dadffff268f5ce1f0b9bff9577d3452f6e
Kyle Brown <cloudspire@hotmail.com>
2017-02-21 16:55:30 -0600

    adding files cache / data-context object

--- jsdb/data_model.js
+++ jsdb/data_model.js
@@ -4,16 +4,91 @@ module.exports.init = function() {
 	//FILE DATABASE
 	global.jsdb.files = new jsondb('jsdb/db_files', true, false);
 	try {
-		global.jsdb.config.getData('/files');
+		global.jsdb.files.getData('/files');
 	} catch (ex) {
 		global.jsdb.files.push('/files', {});
+	}
+	try {
+		global.jsdb.files.getData('/build');
+	} catch (ex) {
+		global.jsdb.files.push('/build', {});
 	}
 	//CONFIGURATION DATABASE
 	global.jsdb.config = new jsondb('jsdb/db_config', true, false);
 	try {
 		global.svr_config = global.jsdb.config.getData('/svr');
 	} catch (ex) {
-		global.jsdb.config.push('/svr', {});
-		global.svr_config = {};
+		global.jsdb.config.push('/svr', {'cache': 'on'});
+		global.svr_config = {'cache': 'on'};
+	}
+}
+
+module.exports.data_context = {
+	files: {
+		get: function() {
+			return get_data('files', '/files');
+		},
+		update: function(data) {
+			apply_changes('files', '/files', data);
+		}
+	},
+	build: {
+		get: function() {
+			return get_data('files', '/build');
+		},
+		update: function(data) {
+			apply_changes('files', '/build', data);
+		}
+	},
+	svr_config: {
+		get: function() {
+			return get_data('config', '/svr');
+		},
+		get_key: function(key) {
+			var data = get_data('config', '/svr');
+			if (data != null) {
+				return data[key];
+			} else {
+				return null;
+			}
+		},
+		set_key: function(key, obj) {
+			var data = get_data('config', '/svr');
+			if (data != null) {
+				data[key] = obj;
+				apply_changes('config', '/svr', data);
+			} else {
+				return null;
+			}
+		},
+		update: function(data) {
+			apply_changes('config', '/svr', data);
+		}
+	}
+}
+
+function get_data(obj, key) {
+	if (obj == null || key == null) {
+		console.log('Error saving changes: null parameters in "data_model.get_data"');
+		return;
+	}
+	try {
+		var rslt = global.jsdb[obj].getData(key);
+		return rslt;
+	} catch (ex) {
+		console.dir(ex);
+		return null;
+	}
+}
+
+function apply_changes(obj, key, data) {
+	if (obj == null || key == null || data == null) {
+		console.log('Error saving changes: null parameters in "data_model.apply_changes"');
+		return;
+	}
+	try {
+		global.jsdb[obj].push(key, data);
+	} catch (ex) {
+		console.dir(ex);
 	}
 }
--- routes/files.js
+++ routes/files.js
@@ -1,23 +1,42 @@
 var express = require('express');
 var router = express.Router();
 var csfs = require("../controllers/file_system");
+var dbcontext = require('../jsdb/data_model').data_context;
 
 router.get('/', function(req, res) {
-	csfs.get_list(global.svr_config.drives)
-		.then((rslt) => { res.json(rslt); })
-		.catch((errs) => {
-			console.log(errs);
-			res.status(500).send('Error retreiving files.')
-		});
+	var cache = dbcontext.svr_config.get_key('cache') == 'on';
+	var cache_loaded = dbcontext.svr_config.get_key('files_cached') == 'true'
+	if (cache && cache_loaded) {
+		console.log('Returning files from cache');
+		var build = dbcontext.files.get();
+		res.json(build);
+	} else {
+		csfs.get_list(global.svr_config.drives)
+			.then(cache_files)
+			.then((rslt) => { res.json(rslt); })
+			.catch((errs) => {
+				console.log(errs);
+				res.status(500).send('Error retreiving files.')
+			});
+	}
 });
 
 router.get('/build', function(req, res) {
-	csfs.get_object(global.svr_config.drives)
-		.then((rslt) => { res.json(rslt); })
-		.catch((errs) => {
-			console.log(errs);
-			res.status(500).send('Error retreiving files.')
-		});
+	var cache = dbcontext.svr_config.get_key('cache') == 'on';
+	var cache_loaded = dbcontext.svr_config.get_key('build_cached') == 'true'
+	if (cache && cache_loaded) {
+		console.log('Returning build from cache');
+		var build = dbcontext.build.get();
+		res.json(build);
+	} else {
+		csfs.get_object(global.svr_config.drives)
+			.then(cache_build)
+			.then((rslt) => { res.json(rslt); })
+			.catch((errs) => {
+				console.log(errs);
+				res.status(500).send('Error retreiving files.')
+			});
+	}
 });
 
 router.all('/mod/*', function(req, res, next) {
@@ -31,3 +50,23 @@ router.post('/mod/rename', function(req, res) {
 });
 
 module.exports = router;
+
+function cache_files(data) {
+	return new Promise((res, err) => {
+		if (dbcontext.svr_config.get_key('cache') == 'on') {
+			dbcontext.build.update(data);
+			dbcontext.svr_config.set_key('files_cached', 'true');
+		}
+		res(data);
+	});
+}
+
+function cache_build(data) {
+	return new Promise((res, err) => {
+		if (dbcontext.svr_config.get_key('cache') == 'on') {
+			dbcontext.files.update(data);
+			dbcontext.svr_config.set_key('build_cached', 'true');
+		}
+		res(data);
+	});
+}
